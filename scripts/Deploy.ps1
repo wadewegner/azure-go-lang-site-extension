bd11f9604df9fc76e33664c9a4832079a13097b3
Wade Wegner <wade.wegner@gmail.com>
2015-01-03 14:20:08 -0700

    Updated unzip to run asynchronously

--- scripts/Deploy.ps1
+++ scripts/Deploy.ps1
@@ -1,10 +1,9 @@
-Set-StrictMode -version 2
 Switch-AzureMode -Name AzureResourceManager
 
 $subName = "Microsoft FTE"
 $userName = "wade@wadewegnergmail.onmicrosoft.com"
 $securePassword = ConvertTo-SecureString -String "PASSWORD" -AsPlainText -Force
-$name = "WadeTest5"
+$name = "WadeTest9"
 $location = "West US"
 $templateFile = "Template.json"
 $siteName = $name + "Site"
@@ -17,6 +16,3 @@ Select-AzureSubscription -SubscriptionName $subName
 New-AzureResourceGroup -Name $name -Location $location -TemplateFile $templateFile -siteName $siteName -hostingPlanName $hostingPlanName -siteLocation $location -Force
  
 Switch-AzureMode -Name AzureServiceManagement
-
-Restart-AzureWebsite –Name $siteName
-Restart-AzureWebsite –Name $siteName

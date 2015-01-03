46d1a366705afaabc514e0ac5a2156b85c6fb286
Wade Wegner <wade.wegner@gmail.com>
2015-01-03 14:18:14 -0700

    Updated unzip to run asynchronously

--- go-lang/install.cmd
+++ go-lang/install.cmd
@@ -5,5 +5,5 @@ IF NOT EXIST "%WEBROOT_PATH%server.go" (
 IF NOT EXIST "%WEBROOT_PATH%go1.4.windows-386.zip" (
 	cd /D %WEBROOT_PATH%
 	curl -O https://storage.googleapis.com/golang/go1.4.windows-386.zip
-	unzip -o go1.4.windows-386.zip -d .
+	start unzip -o go1.4.windows-386.zip -d .
 )

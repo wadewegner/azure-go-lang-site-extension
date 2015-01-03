167fe44b87a3271f68c31841c11e6564a15c7aef
Wade Wegner <wade.wegner@gmail.com>
2015-01-02 23:58:10 -0700

    Fix server.go

--- go-lang/server.go
+++ go-lang/server.go
@@ -1,13 +1,16 @@
 package main
+
 import (
     "fmt"
     "net/http"
     "os" 
 )
+
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "You just browsed page (if blank you're at the root): %s", r.URL.Path[1:])
 }
+
 func main() {
     http.HandleFunc("/", handler)
-    http.ListenAndServe(":"+os.Getenv("HTTPPLATFORMPORT"), nil)
+    http.ListenAndServe(":"+os.Getenv("HTTP_PLATFORM_PORT"), nil)
 }

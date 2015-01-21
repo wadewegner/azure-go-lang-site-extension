b306f032a6e75145170d778e3feab4f98f80baaf
Wade Wegner <wade.wegner@gmail.com>
2015-01-20 20:13:00 -0700

    Reverted to primary download

--- golang/install.cmd
+++ golang/install.cmd
@@ -4,6 +4,6 @@ IF NOT EXIST "%WEBROOT_PATH%server.go" (
 
 IF NOT EXIST "%WEBROOT_PATH%go1.4.windows-386.zip" (
 	cd /D %WEBROOT_PATH%
-	curl -O http://wadescratch.blob.core.windows.net/public/go1.4.1.windows-386-waw.zip
-	start unzip -o go1.4.1.windows-386-waw.zip -d .
+	curl -O https://storage.googleapis.com/golang/go1.4.1.windows-386.zip
+	start unzip -o go1.4.1.windows-386.zip -d .
 )
